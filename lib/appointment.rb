7733f797a6248a5d04a076beafec2256dba07267
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-09 01:17:42 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,9 +4,9 @@ class Appointment
   @@all = []
   
     def initialize(date, patient, doctor)
-        @date = date @doctor = doctor
+        @date = date 
       @patient = patient  
-    
+    @doctor = doctor
       @@all << self
     end
   
